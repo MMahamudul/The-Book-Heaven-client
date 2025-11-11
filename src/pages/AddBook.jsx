ea125e524265a58a931479f17163f32e92bcf276
mahamudul-alam <alam.mmahamudul@gmail.com>
2025-11-11 14:36:05 +0200

    Add book modifie

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import useAxios from "../hooks/useAxios";
 
 const AddBook = () => {
+    const instance = useAxios();
+
+    const handleAddBook = (e) =>{
+        e.preventDefault();
+
+        const title = e.target.title.value;
+        const author = e.target.author.value;
+        const genre = e.target.genre.value;
+        const rating = e.target.rating.value;
+        const summary = e.target.summary.value;
+        const photo = e.target.photo.value;
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+
+        const newBook = {title, author, genre, rating, summary, name , email, photo}
+
+       /*  console.log(title, author, genre, rating, summary, name , email, photo) */
+
+       instance.post('/add-book', newBook)
+       .then(data => console.log(data.data))
+
+    }
+
+
+
   return (
     <div className="min-h-screen bg-base-100 flex items-center justify-center p-6">
       <div className="card w-full max-w-2xl shadow-2xl bg-base-200 border border-accent/40">
@@ -9,7 +35,7 @@ const AddBook = () => {
             Add a New Book
           </h2>
 
-          <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <form onSubmit={handleAddBook} className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* Title */}
             <div className="form-control">
               <label className="label">
@@ -17,8 +43,10 @@ const AddBook = () => {
               </label>
               <input
                 type="text"
+                name= 'title'
                 placeholder="Name of the Book"
                 className="input input-bordered border-2 border-accent/60 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                required
               />
             </div>
 
@@ -29,8 +57,10 @@ const AddBook = () => {
               </label>
               <input
                 type="text"
+                name= 'author'
                 placeholder="Author name"
                 className="input input-bordered border-2 border-accent/60 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                required
               />
             </div>
 
@@ -41,8 +71,10 @@ const AddBook = () => {
               </label>
               <input
                 type="text"
+                name= 'genre'
                 placeholder="e.g., Fiction, Fantasy, Non-Fiction"
                 className="input input-bordered border-2 border-accent/70 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                required
               />
             </div>
 
@@ -53,9 +85,11 @@ const AddBook = () => {
               </label>
               <input
                 type="number"
+                name= 'rating'
                 step="0.1"
                 placeholder="1–5"
                 className="input input-bordered border-2 border-accent/70 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                required
               />
             </div>
 
@@ -66,7 +100,9 @@ const AddBook = () => {
               </label>
               <textarea
                 placeholder="Write a short summary of the book..."
+                name= 'summary'
                 className="textarea textarea-bordered border-2 border-accent/70 h-28 focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                required
               ></textarea>
             </div>
 
@@ -76,9 +112,11 @@ const AddBook = () => {
                 <span className="label-text font-bold my-2">Cover Image URL</span>
               </label>
               <input
-                type="url"
+                type="text"
+                name= 'photo'
                 placeholder="https://example.com/image.jpg"
                 className="input input-bordered border-2 border-accent/70 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                required
               />
             </div>
 
@@ -89,8 +127,10 @@ const AddBook = () => {
               </label>
               <input
                 type="email"
+                name= 'email'
                 placeholder="example@email.com"
                 className="input input-bordered border-2 border-accent/70 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
+                
               />
             </div>
 
@@ -101,6 +141,7 @@ const AddBook = () => {
               </label>
               <input
                 type="text"
+                name= 'name'
                 placeholder="Added by..."
                 className="input input-bordered border-2 border-accent/70 w-full focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent"
               />
